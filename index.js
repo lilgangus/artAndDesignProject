ca82d17ef37198ed7c96fe00cb307757490a39d1
lilgangus <tangcharles29@gmail.com>
2023-08-14 20:52:30 -0700

    room kinda moves

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
-const exhibits = document.querySelectorAll('.exhibit');
-let currentExhibit = 0;
+const line1 = document.getElementById("line1")
+const line2 = document.getElementById("line2")
+const line3 = document.getElementById("line3")
+const line4 = document.getElementById("line4")
+const line5 = document.getElementById("line5")
+const line6 = document.getElementById("line6")
+const line7 = document.getElementById("line7")
 
-window.addEventListener('scroll', () => {
-    const scrollPosition = window.pageYOffset;
-    const exhibitOffset = exhibits[currentExhibit].offsetTop;
+const line8 = document.getElementById("line8")
+const line9 = document.getElementById("line9")
 
-    if (scrollPosition >= exhibitOffset) {
-        currentExhibit++;
-        if (currentExhibit < exhibits.length) {
-            window.scrollTo({ top: exhibits[currentExhibit].offsetTop, behavior: 'smooth' });
-        }
+function animateRoom() {
+    // when scrolled, the line will move downwards
+    let scroll = window.scrollY;
+    if(scroll < 150) {
+        line1.style.top = `${scroll * 0.5}px`;
+        // line1.style.transform = `translateY(${scroll * 0.5}px)`;
+        line2.style.transform = `translateY(${scroll * 0.5}px)`;
+        line3.style.transform = `translateY(${scroll * 0.5}px)`;
+        line4.style.transform = `translateY(${scroll * 0.5}px)`;
+    
+        line5.style.transform = `translateY(${scroll * 0.3}px)`;
+        line6.style.transform = `translateY(${scroll * 0.3}px)`;
+        line6.style.height = `${40 + scroll * 0.025}%`;
+        line7.style.transform = `translateY(${scroll * 0.3}px)`;
+        line7.style.height = `${40 + scroll * 0.025}%`;
+
+        line8.style.transform = `translatex(${scroll * 0.3}px)`;
+    } else {
+        line1.style.transform = `translateY(0px)`;
+        line2.style.transform = `translateY(0px)`;
+        line3.style.transform = `translateY(0px)`;
+        line4.style.transform = `translateY(0px)`;
+    
+        line5.style.transform = `translateY(0px)`;
+        line6.style.transform = `translateY(0px)`;
+        line6.style.height = `40%`;
+        line7.style.transform = `translateY(0px)`;
+        line7.style.height = `40%`;
     }
-});
+  
+
+    console.log(scroll);
+
+    
+
+}
+
+window.addEventListener("scroll", animateRoom);
