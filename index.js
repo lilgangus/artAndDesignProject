23297d1203ae1370b1bc3b21d83c590c09e46870
lilgangus <tangcharles29@gmail.com>
2023-08-14 21:34:42 -0700

    start of the museum

--- index.js
+++ index.js
@@ -11,32 +11,44 @@ const line9 = document.getElementById("line9")
 
 function animateRoom() {
     // when scrolled, the line will move downwards
-    let scroll = window.scrollY;
-    if(scroll < 150) {
-        line1.style.top = `${scroll * 0.5}px`;
-        // line1.style.transform = `translateY(${scroll * 0.5}px)`;
-        line2.style.transform = `translateY(${scroll * 0.5}px)`;
-        line3.style.transform = `translateY(${scroll * 0.5}px)`;
-        line4.style.transform = `translateY(${scroll * 0.5}px)`;
-    
-        line5.style.transform = `translateY(${scroll * 0.3}px)`;
-        line6.style.transform = `translateY(${scroll * 0.3}px)`;
-        line6.style.height = `${40 + scroll * 0.025}%`;
-        line7.style.transform = `translateY(${scroll * 0.3}px)`;
-        line7.style.height = `${40 + scroll * 0.025}%`;
+    let scroll = window.scrollY + 1;
+    if(scroll < 75) {
+        line1.style.top = `${0.5 + scroll * 0.01}%`;
+        line2.style.top = `${7.5 + scroll * 0.01}%`;
+        line3.style.top = `${0.5 + scroll * 0.01}%`;
+        line4.style.top = `${0.5 + scroll * 0.01}%`;
 
-        line8.style.transform = `translatex(${scroll * 0.3}px)`;
+        line6.style.height = `${4.5 + scroll * 0.01}%`;
+        line7.style.height = `${4.5 + scroll * 0.01}%`;
+
+        line8.style.top = `${scroll * 0.0166}%`;
+        line8.style.left = `${20 + scroll * 0.066}%`;
+
+        line9.style.top = `${scroll * 0.0166}%`;
+        line9.style.left = `${80 - scroll * 0.066}%`;
+
+        line10.style.top = `${scroll * 0.0166}%`;
+        line10.style.left = `${20 + scroll * 0.066}%`;
+        line10.style.width = `${60 - scroll * 0.132}%`;
+        
+
+        // line5.style.transform = `translateY(${scroll * 0.3}px)`;
+        // line6.style.transform = `translateY(${scroll * 0.3}px)`;
+        // line6.style.height = `${40 + scroll * 0.025}%`;
+        // line7.style.transform = `translateY(${scroll * 0.3}px)`;
+        // line7.style.height = `${40 + scroll * 0.025}%`;
+
     } else {
-        line1.style.transform = `translateY(0px)`;
-        line2.style.transform = `translateY(0px)`;
-        line3.style.transform = `translateY(0px)`;
-        line4.style.transform = `translateY(0px)`;
+        // line1.style.transform = `translateY(0px)`;
+        // line2.style.transform = `translateY(0px)`;
+        // line3.style.transform = `translateY(0px)`;
+        // line4.style.transform = `translateY(0px)`;
     
-        line5.style.transform = `translateY(0px)`;
-        line6.style.transform = `translateY(0px)`;
-        line6.style.height = `40%`;
-        line7.style.transform = `translateY(0px)`;
-        line7.style.height = `40%`;
+        // line5.style.transform = `translateY(0px)`;
+        // line6.style.transform = `translateY(0px)`;
+        // line6.style.height = `40%`;
+        // line7.style.transform = `translateY(0px)`;
+        // line7.style.height = `40%`;
     }
   
 
