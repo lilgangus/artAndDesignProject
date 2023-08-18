35302d06dc9a3b844d923eabe488a7ec3513a363
lilgangus <67040185+lilgangus@users.noreply.github.com>
2023-08-18 14:12:36 -0700

    basic test done (shadows and colors look funky)

--- artDesignProj/main.js
+++ artDesignProj/main.js
@@ -2,6 +2,8 @@ import './style.css'
 
 import * as THREE from 'three'
 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
 const scence = new THREE.Scene()
 
 // the first parameter is the field of view, the second is the aspect ratio, the third is the near clipping plane, and the fourth is the far clipping plane.
@@ -22,14 +24,51 @@ camera.position.setZ(30)
 renderer.render(scence, camera)
 
 const geometry = new THREE.TorusGeometry(10,3,16,100)
-const material = new THREE.MeshBasicMaterial({color: 0xFF6347, wireframe: true})
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347})
 const torus = new THREE.Mesh(geometry, material)
 
 scence.add(torus)
 
+const pointLight = new THREE.PointLight(0xffffff, 100)
+pointLight.position.set(0,0,0)
+
+const ambientLight = new THREE.AmbientLight(0xffffff)
+scence.add(pointLight, ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200,50)
+scence.add(lightHelper, gridHelper)
+
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.5, 32, 32)
+  const material = new THREE.MeshStandardMaterial({color: 0xFFFFFF})
+  const star = new THREE.Mesh(geometry, material)
+
+  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100))
+
+  star.position.set(x,y,z)
+  scence.add(star)
+}
+
+Array(200).fill().forEach(addStar)
+
+const spaceTexture = new THREE.TextureLoader().load('space.jpg')
+scence.background = spaceTexture
+
 // created animate function to render the scene again every time there is a screen refresh
 function animate() {
   requestAnimationFrame(animate)
+
+  torus.rotation.x += 0.01
+  torus.rotation.y += 0.005
+  torus.rotation.z += 0.01
+
+  controls.update()
+
+
   renderer.render(scence, camera)
 }
 
